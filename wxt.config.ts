88d7053c43f9e51e8d874c7544188241290ec62b
Robpol86 <robpol86@gmail.com>
2025-04-24 15:37:59 +0200

    Fix missing icons and logo (#5)
    * manifestVersion: 3
    
    * Fixed
    
    Template has the wrong path for public directory.
    
    Fixed icon in Chrome and Firefox, and fixed broken image in popup in
    Chrome. Popup still broken in Firefox.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
+  manifestVersion: 3,
   modules: ['@wxt-dev/module-svelte'],
 });
