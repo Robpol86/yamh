1c4465631221b8b021d6d2bafd5372811fcabc19
Robpol86 <robpol86@gmail.com>
2025-04-25 12:25:30 +0200

    silence lint from template code

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -3,6 +3,7 @@ import App from "./App.svelte";
 import { mount } from "svelte";
 
 const app = mount(App, {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     target: document.getElementById("app")!,
 });
 
