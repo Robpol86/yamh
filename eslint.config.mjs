ea934af04db7fbbc3ca744a73c7d54cfb0744914
Robpol86 <robpol86@gmail.com>
2025-04-25 12:31:24 +0200

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,6 @@ export default tseslint.config(
                 projectService: {
                     allowDefaultProject: ["eslint.config.mjs"],
                 },
-                tsconfigRootDir: import.meta.dirname,
             },
         },
         rules: {
