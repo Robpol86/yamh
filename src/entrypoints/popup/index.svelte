b2ba59ab62be48ded1e6cd2a3892c127ae857463
Robpol86 <robpol86@gmail.com>
2025-05-06 15:12:12 +0200

    enabled svelte linting

--- src/entrypoints/popup/index.svelte
+++ src/entrypoints/popup/index.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import { i18n } from '#i18n';
+    import { i18n } from "#i18n";
 </script>
+
+<form>
+    <fieldset>
+        <input type="checkbox" role="switch" id="enableChbx" disabled />
+        <label for="enableChbx" id="enableChbxLabel">{i18n.t("websiteNotSupported")}</label>
+    </fieldset>
+</form>
 
 <style>
 </style>
-
-<form>
-  <fieldset>
-    <input type="checkbox" role="switch" id="enableChbx" disabled>
-    <label for="enableChbx" id="enableChbxLabel">{i18n.t("websiteNotSupported")}</label>
-  </fieldset>
-</form>
