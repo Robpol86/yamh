b8c5fe7717c76e156dc4ef76f15148932538b59b
Robpol86 <robpol86@gmail.com>
2025-05-07 16:11:54 +0200

    Adding `make build` (#20)
    * Adding `make build`
    
    I kept running the pnpm command manually a lot, especially to regenerate
    the .wxt directory.
    
    Moving removal of .wxt into `make clean` since build/dev will regenerate
    it. distclean is too expensive what with the removal of node_modules.
    
    Sorting wxt.config.ts
    
    Adding `make dev` shortcut.
    
    * make all builds

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    srcDir: "src",
     manifestVersion: 3,
     modules: ["@wxt-dev/module-svelte"],
+    srcDir: "src",
 });
